62325497355c2faea2de1298ed843cfee8f66153
Dusan-Merdzanovski <dusan.merdzanovski@gmail.com>
2023-09-19 02:12:00 +0200

    Add ReviewsController and Views

---
+++ MVCFilmTicketStore/Controllers/ReviewsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCFilmTicketStore.Data;
+using MVCFilmTicketStore.Models;
+
+namespace MVCFilmTicketStore.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly MVCFilmTicketStoreContext _context;
+
+        public ReviewsController(MVCFilmTicketStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            var mVCFilmTicketStoreContext = _context.Review.Include(r => r.Film);
+            return View(await mVCFilmTicketStoreContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Review == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.Film)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["FilmId"] = new SelectList(_context.Film, "Id", "Title");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AppUser,Comment,Rating,FilmId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmId"] = new SelectList(_context.Film, "Id", "Title", review.FilmId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Review == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["FilmId"] = new SelectList(_context.Film, "Id", "Title", review.FilmId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUser,Comment,Rating,FilmId")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmId"] = new SelectList(_context.Film, "Id", "Title", review.FilmId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Review == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.Film)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Review == null)
+            {
+                return Problem("Entity set 'MVCFilmTicketStoreContext.Review'  is null.");
+            }
+            var review = await _context.Review.FindAsync(id);
+            if (review != null)
+            {
+                _context.Review.Remove(review);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+          return (_context.Review?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
